8e9f42478f67cd0547067a06ee8a98a2c35c7146
Austin <21083126+austin-vu1017@users.noreply.github.com>
2022-04-25 02:55:07 -0500

    Update main.py
    Added find_password(), add "Search" button, and realign all widgets including logo.

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-import email
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 from pyperclip import copy
 import json
 
+# search info
+def find_password():
+    website = website_input.get()
+    
+    try:
+        with open("data.json", mode="r") as output:
+            data = json.load(output)
+    except FileNotFoundError:
+        messagebox.showerror(title="Oops", message="No Data File Found")
+    else:
+        if website in data:
+            # store the key's in the website searched into a list. info[0] is email, info[1] is password
+            info = [web for web in data[website].values()]
+            messagebox.showinfo(title=website, message=f"Email: {info[0]} \nPassword: {info[1]}")
+        else:
+            messagebox.showerror(title="Oops", message=f"No details for {website} exists")
 # password generator
 def generate_pw():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -57,7 +72,6 @@ def save():
                 website_input.delete(0, END)
                 user_input.delete(0, END)
                 password_input.delete(0, END)
-                
 # UI setup
 window = Tk()
 window.title("Password Manager")
@@ -65,7 +79,7 @@ window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=logo_img)
+canvas.create_image(135, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
 # Labels
@@ -77,16 +91,18 @@ password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
 # Inputs
-website_input = Entry(width=35)
-website_input.grid(column=1, row=1, columnspan=2)
-user_input = Entry(width=35)
+website_input = Entry(width=21)
+website_input.grid(column=1, row=1)
+user_input = Entry(width=38)
 user_input.grid(column=1, row=2, columnspan=2)
 password_input = Entry(width=21)
 password_input.grid(column=1, row=3)
 
 # Buttons
-generate_pw = Button(text="Generate Password", command=generate_pw)
-generate_pw.grid(column=2, row=3)
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(column=2, row=1)
+generate_password = Button(text="Generate Password", command=generate_pw)
+generate_password.grid(column=2, row=3)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
